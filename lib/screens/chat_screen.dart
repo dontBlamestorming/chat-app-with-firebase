b741651a060b0ac95e493ebc076cddcba8f535ec
ukdong <lklone20005@gmail.com>
2022-02-13 23:33:28 +0900

    FEAT: Add Sign out

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -37,6 +37,18 @@ class _ChatScreenState extends State<ChatScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Chat Screen"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _authentication.signOut();
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.exit_to_app_sharp,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
       body: const Center(
         child: Text("Chat Screen"),
